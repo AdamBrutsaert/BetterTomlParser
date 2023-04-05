e58398588341473c3215e31836c1fdfb4d87c8b2
Adam Brutsaert <adam.brutsaert@epitech.eu>
2023-04-05 16:38:56 +0200

    [+] Freeing tokens from lexer

--- src/toml/lexer/toml_lexer_delete.c
+++ src/toml/lexer/toml_lexer_delete.c
@@ -13,6 +13,8 @@ void toml_lexer_delete(toml_lexer_t *lexer)
 {
     if (lexer == NULL)
         return;
+    for (size_t i = 0; i < lexer->length; i++)
+        toml_token_delete(lexer->tokens[i]);
     free(lexer->tokens);
     free(lexer);
 }
